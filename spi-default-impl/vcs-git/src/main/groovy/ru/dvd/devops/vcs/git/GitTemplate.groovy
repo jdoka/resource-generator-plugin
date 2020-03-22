9b97bd781a1f3aa3e9ad0e820a586304e4a273f9
dvdruzhinin <dmitry.drujinin.prof@gmail.com>
2020-03-22 15:52:13 +0700

    GitTemplate refactoring ++

--- spi-default-impl/vcs-git/src/main/groovy/ru/dvd/devops/vcs/git/GitTemplate.groovy
+++ spi-default-impl/vcs-git/src/main/groovy/ru/dvd/devops/vcs/git/GitTemplate.groovy
@@ -16,6 +16,7 @@ import java.util.stream.Collectors
 class GitTemplate<T> implements VcsOperations<T> {
 
     private static final Logger logger = LoggerFactory.getLogger(GitTemplate.class)
+    private static final String TAGS_PREFIX = "refs/tags/"
 
     private Git git
     private RevWalk walk
@@ -38,36 +39,16 @@ class GitTemplate<T> implements VcsOperations<T> {
     @Override
     T doInTaggedCommit(Closure<T> action, T defaultValue, String... tags) {
         git.gc().call()
-        RevCommit lastCommit = getLastCommit()
         RevCommit lastTaggedCommit = getLastTaggedCommit(tags)
+        RevCommit lastCommit = getLastCommit()
         Objects.nonNull(lastTaggedCommit) ? doInCommit(lastTaggedCommit, lastCommit, action) : defaultValue
-    }
-
-    /**
-     * Возвращает последний коммит текущей ветки.
-     */
-    private RevCommit getLastCommit() {
-        ObjectId headId = git.getRepository().resolve(Constants.HEAD)
-        walk.parseCommit(headId)
     }
 
     /**
      * Возвращает последний коммит, теггированный tagNames. Если tagNames пусто, то любым тегом.
      */
     private RevCommit getLastTaggedCommit(String... tagNames) {
-        List<Ref> tags = git.tagList().call()
-        logger.trace("Total tags: {}", tags.size())
-        Set<ObjectId> taggedCommitIds = tags.stream()
-                .map({ walk.parseTag(it.objectId) })
-                .filter({ tagNames.toList().isEmpty() ? true : tagNames.contains(it.tagName) })
-                .map({
-                    def refTag = git.repository.findRef("refs/tags/" + it.tagName)
-                    refTag.peeledObjectId
-                })
-                .filter({ Objects.nonNull(it) })
-                .collect(Collectors.toSet())
-
-        //идем по истории коммитов, находим последний теггированный.
+        def taggedCommitIds = getTaggedCommitIds(tagNames)
         RevCommit currentCommit = lastCommit.getParent(0)
         while (currentCommit) {
             currentCommit = walk.parseCommit(currentCommit.toObjectId())
@@ -82,6 +63,28 @@ class GitTemplate<T> implements VcsOperations<T> {
                 return null
             }
         }
+    }
+
+    private Collection<ObjectId> getTaggedCommitIds(String... tagNames) {
+        List<Ref> tags = git.tagList().call()
+        logger.trace("Total tags: {}", tags.size())
+        tags.stream()
+                .map({ walk.parseTag(it.objectId) })
+                .filter({ tagNames.toList().isEmpty() ? true : tagNames.contains(it.tagName) })
+                .map({
+                    def refTag = git.repository.findRef(TAGS_PREFIX + it.tagName)
+                    refTag.peeledObjectId
+                })
+                .filter({ Objects.nonNull(it) })
+                .collect(Collectors.toSet())
+    }
+
+    /**
+     * Возвращает последний коммит текущей ветки.
+     */
+    private RevCommit getLastCommit() {
+        ObjectId headId = git.getRepository().resolve(Constants.HEAD)
+        walk.parseCommit(headId)
     }
 
     /**
